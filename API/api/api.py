874170a416f4fba903954c2537a44e1f8be2d929
Rubén Castro González <prod_ingruben@hotmail.com>
2023-03-03 13:22:58 -0600

    Se agrega la letra "s" por el cambio de servidor
    Se modifica la api para el cambio de servidor

--- API/api/api.py
+++ API/api/api.py
@@ -30,7 +30,7 @@ user = "admin"
 password = "4dm1n_001"
 database = "eiaf"
 # Credenciales para permitir conexión con Servidor de FAMX2 (Sistema de Trazabilidad)
-serverp2 = "naapnx-famx2"
+serverp2 = "naapnx-famx2s"
 dbp2 = "agrucomb_prod"
 userp2 = "pnx_agrucomb_prod"
 passwordp2 = "pJ0rge2021"
